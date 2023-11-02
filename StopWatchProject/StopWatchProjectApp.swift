ba24a8b4b559e857e033d49fc787cea1b3466064
Josh Yim <jyim@lcs.on.ca>
2023-11-02 18:47:49 -0400

    Initial Commit

---
+++ StopWatchProject/StopWatchProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopWatchProjectApp.swift
+//  StopWatchProject
+//
+//  Created by junxi Yim on 2023/11/2.
+//
+
+import SwiftUI
+
+@main
+struct StopWatchProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
