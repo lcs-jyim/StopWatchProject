a9af1419ec2b4576f643adb94049133eb1f0d819
Josh Yim <jyim@lcs.on.ca>
2023-11-02 20:03:30 -0400

    created another button view file to simplify my code

---
+++ StopWatchProject/Button View.swift
@@ -0,0 +1,8 @@
+//
+//  Button View.swift
+//  StopWatchProject
+//
+//  Created by junxi Yim on 2023/11/2.
+//
+
+import Foundation
---
+++ StopWatchProject/ButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonView.swift
+//  StopWatchProject
+//
+//  Created by junxi Yim on 2023/11/2.
+//
+
+import SwiftUI
+
+struct ButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonView()
+}
